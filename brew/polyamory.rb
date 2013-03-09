28c43ff60dd3d78cddab9d1752444aec73c82718
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-03-08 20:39:43 -0500

    update formula

--- brew/polyamory.rb
+++ brew/polyamory.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Polyamory < Formula
   homepage 'https://github.com/mislav/polyamory#readme'
-  url      'https://github.com/mislav/polyamory/tarball/v0.6.0'
-  sha1     'c025cc220a553208930450fc8cfc78a6157d15d3'
+  url      'https://github.com/mislav/polyamory/tarball/v0.6.2'
+  sha1     'c4d63cc22b58d89a8cbea1cd22870d1f1bfe8ac3'
   head     'https://github.com/mislav/polyamory.git'
 
   def install
